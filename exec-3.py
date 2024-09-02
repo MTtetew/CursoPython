43f1e2cae025a6d22cdb9d36896362c30fd3b4ff
MTtetew <zeusmatheus28@gmail.com>
2024-09-02 19:39:00 -0300

    exec-3.py

--- exec-3.py
+++ exec-3.py
@@ -18,3 +18,8 @@ print(C)
 
 C=4+2*2-4
 print(C)
+
+
+Explicação:
+B=O resultado deu diferente pos o computador segue a conta apartir dos parenteses.
+C=O primeiro resultado ele segue primeiro apartir do parentese,e o segundo resultado e apartir da multiplicação.
